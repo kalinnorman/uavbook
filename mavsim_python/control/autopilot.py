0f5b4a27ff5fee35413c5aa412ace5f1d9914357
Kalin Norman <kalinnorman@gmail.com>
2023-02-18 00:42:55 -0700

    Initial solution for chap 6

--- mavsim_python/control/autopilot.py
+++ mavsim_python/control/autopilot.py
@@ -62,21 +62,30 @@ class Autopilot:
 	
 	#### TODO #####
         # lateral autopilot
-
+        chi_c = wrap(cmd.course_command, state.chi)
+        phi_c = self.saturate(cmd.phi_feedforward + \
+                              self.course_from_roll.update(chi_c, state.chi), \
+                              -np.radians(30), np.radians(30))
+        delta_a = self.roll_from_aileron.update(phi_c ,state.phi ,state.p)
+        delta_r = self.yaw_damper.update(state.r)
 
         # longitudinal autopilot
-
+        h_c = self.saturate(cmd.altitude_command, state.altitude - AP.altitude_zone, 
+                            state.altitude + AP.altitude_zone)
+        theta_c = self.altitude_from_pitch.update(h_c, state.altitude)
+        delta_e = self.pitch_from_elevator.update(theta_c, state.theta, state.q)
+        delta_t = self.airspeed_from_throttle.update(cmd.airspeed_command, state.Va)
 
         # construct control outputs and commanded states
-        delta = MsgDelta(elevator=0,
-                         aileron=0,
-                         rudder=0,
-                         throttle=0)
-        self.commanded_state.altitude = 0
-        self.commanded_state.Va = 0
-        self.commanded_state.phi = 0
-        self.commanded_state.theta = 0
-        self.commanded_state.chi = 0
+        delta = MsgDelta(elevator=delta_e,
+                         aileron=delta_a,
+                         rudder=delta_r,
+                         throttle=delta_t)
+        self.commanded_state.altitude = cmd.altitude_command
+        self.commanded_state.Va = cmd.airspeed_command
+        self.commanded_state.phi = phi_c
+        self.commanded_state.theta = theta_c
+        self.commanded_state.chi = chi_c
         return delta, self.commanded_state
 
     def saturate(self, input, low_limit, up_limit):
--- mavsim_python/parameters/control_parameters.py
+++ mavsim_python/parameters/control_parameters.py
@@ -13,37 +13,37 @@ sigma = 0  # low pass filter gain for derivative
 
 #----------roll loop-------------
 # get transfer function data for delta_a to phi
-wn_roll = 0
-zeta_roll = 0
-roll_kp = 0
-roll_kd = 0
+wn_roll = 20
+zeta_roll = 0.707
+roll_kp = wn_roll**2 / TF.a_phi2
+roll_kd = (2 * zeta_roll * wn_roll - TF.a_phi1) / TF.a_phi2
 
 #----------course loop-------------
-wn_course = 0
-zeta_course = 0
-course_kp = 0
-course_ki = 0
+wn_course = (1 / 20) * wn_roll
+zeta_course = 1.0
+course_kp = 2 * zeta_course * wn_course * Va0 / gravity
+course_ki = wn_course**2 * Va0 / gravity
 
 #----------yaw damper-------------
-yaw_damper_p_wo = 0
-yaw_damper_kr = 0
+yaw_damper_p_wo = 0.45
+yaw_damper_kr = 0.2
 
 #----------pitch loop-------------
-wn_pitch = 0
-zeta_pitch = 0 
-pitch_kp = 0
-pitch_kd = 0
-K_theta_DC = 0
+wn_pitch = 20
+zeta_pitch = 0.707
+pitch_kp = (wn_pitch**2 - TF.a_theta2) / TF.a_theta3
+pitch_kd = (2 * zeta_pitch * wn_pitch - TF.a_theta1) / TF.a_theta3
+K_theta_DC = pitch_kp * TF.a_theta3 / (TF.a_theta2 + pitch_kp * TF.a_theta3)
 
 #----------altitude loop-------------
-wn_altitude = 0
-zeta_altitude = 0
-altitude_kp = 0
-altitude_ki = 0
-altitude_zone = 0
+wn_altitude = (1 / 20) * wn_pitch
+zeta_altitude = 1
+altitude_kp = (2 * zeta_altitude * wn_altitude) / (K_theta_DC * Va0)
+altitude_ki = wn_altitude**2 / (K_theta_DC * Va0)
+altitude_zone = 10
 
 #---------airspeed hold using throttle---------------
-wn_airspeed_throttle = 0
-zeta_airspeed_throttle = 0
-airspeed_throttle_kp = 0
-airspeed_throttle_ki = 0
+wn_airspeed_throttle = 1
+zeta_airspeed_throttle = 0.707
+airspeed_throttle_kp = (2 * zeta_airspeed_throttle * wn_airspeed_throttle - TF.a_V1) / TF.a_V2
+airspeed_throttle_ki = wn_airspeed_throttle**2 / TF.a_V2
